d5f63e7baf40373a4966ec18a5bd66a3eb3b96ae
danielgichuki-sudo <daniel.gichuki@gmail.com>
2026-07-10 18:27:48 +0300

    add bascript.sh

---
+++ bashscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script creates a report of our disk configuration
+
+FILENAME=$(hostname)
+echo "Disk report saved to $FILENAME.report"
+
+lvscan >$FILENAME.report
+Vgscan >$FILENAME. report
+pvscan >>$FILENAME.report
+lsblk >$FILENAME.report
+df -hT | grep -v tmp >$FILENAME.report
