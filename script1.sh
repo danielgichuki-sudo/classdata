b73761f9fca2bc485d24500430ecc10d9429dd94
danielgichuki-sudo <daniel.gichuki@gmail.com>
2026-07-11 12:45:57 +0300

    script1.sh
    modification to script1

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@
 FILENAME=$(hostname)
 echo "Disk report saved to $FILENAME.report1"
 
-nlvscan >$FILENAME.report1
+lvscan >$FILENAME.report1
 vgscan >>$FILENAME.report1
 pvscan >>$FILENAME.report1
 lsblk >>$FILENAME.report1
